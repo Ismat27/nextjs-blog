44331679bbc1843853f25664c17b103afede4283
ismat27 <adegbiteismail27@gmail.com>
2023-03-25 14:13:07 +0100

    enabled ssr for react-bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/global.css';
 import '../styles/prism-style.css';
+import { SSRProvider } from 'react-bootstrap';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <SSRProvider>
+            <Component {...pageProps} />
+        </SSRProvider>
+    );
 }
