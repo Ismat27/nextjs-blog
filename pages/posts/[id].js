5ccc1618a145fddce591c737db9601d8e9f3945e
ismat27 <adegbiteismail27@gmail.com>
2023-03-27 21:34:14 +0100

    update structure

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -6,6 +6,7 @@ import utilStyles from '../../styles/utils.module.css';
 import ReactMarkdown from "react-markdown";
 import { useState, useEffect } from "react";
 import Prism from 'prismjs'
+import { Container } from "react-bootstrap";
 
 export async function getStaticPaths() {
     const paths = getAllPostIds()
@@ -37,15 +38,17 @@ export default function Post({postData}) {
             <Head>
                 <title>{postData.title}</title>
             </Head>
-            <article>
-                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-                <div className={utilStyles.lightText}>
-                <Date dateString={postData.date} />
-                </div>
-                <div className="blog">
-                    {hydration && <ReactMarkdown children={postData.matterResult}/> }
-                </div>
-            </article>
+            <Container className="post-detail">
+                <article>
+                    <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                    <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                    </div>
+                    <div className="blog">
+                        {hydration && <ReactMarkdown children={postData.matterResult}/> }
+                    </div>
+                </article>
+            </Container>
         </Layout>
     )
 }
