8f117c36bacd28e8ec8d2ea2669658bae28250f6
ismat27 <adegbiteismail27@gmail.com>
2023-03-27 18:18:24 +0100

    projects data placeholder file

---
+++ projects_data.js
@@ -0,0 +1,39 @@
+const projects_data = [
+    {
+        id: '1',
+        image: '/images/smart_farm_tool.png',
+        title: 'Smart Farm Tools',
+        intro: "A Platform for plants' diseases diagnosis and to connect farmers with agricultural experts for seemless consultancy services",
+        tools: ['React', 'Styled-Compnennts']
+    },
+    {
+        id: '2',
+        image: '/images/smart_mart.png',
+        title: 'Smart Mart',
+        intro: 'Modern day ecommerce web application for shopping different kinds of goods and commodities',
+        tools: ['React','Styled-Components']
+    },
+    {
+        id: '3',
+        image: '/images/where_in_the_world.png',
+        title: 'Where in the world',
+        intro: 'This is an app for obtaining information on  countries demograhic data',
+        tools: ['NextJs']
+    },
+    {
+        id: '4',
+        image: '/images/quizzical_home.png',
+        title: 'Quzzical',
+        intro: 'A quiz app, intended to serve user as a means of passing time while also catching fun.',
+        tools: ['React']
+    },
+    {
+        id: '5',
+        image: '/images/to_do_app.png',
+        title: 'TODO',
+        intro: 'A minimal TODO app for listing and updating tasks to be completed',
+        tools: ['React']
+    },
+]
+
+export {projects_data}
