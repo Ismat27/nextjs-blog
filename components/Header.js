72b71985aa1b55ceb2a1b59b6d182c7fe8581733
ismat27 <adegbiteismail27@gmail.com>
2023-03-27 21:37:41 +0100

    updates

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,20 @@
 import Link from "next/link"
+import { useState } from "react"
 import { Container } from "react-bootstrap"
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
   return (
     <header>
         <nav>
-            <Container>
-                <div className="nav-list-box">
+            <Container className="nav-container">
+                <div className="menu-box">
+                    <h1 className="logo">smart<span>Dev</span></h1>
+                    <div onClick={() => setMenuOpen(!menuOpen)} className="menu">
+                        <button>menu</button>
+                    </div>
+                </div>
+                <div className={`${menuOpen ? "nav-list-box open" : "nav-list-box"}`}>
                     <ul className="nav-list">
                         <li className="nav-list-item">
                             <Link className="nav-link" href={'/'}>
