25acde446d22d242a6152fc2ad3656da17e16be4
ismat27 <adegbiteismail27@gmail.com>
2023-03-30 21:08:47 +0100

    set up functionality to process user message

---
+++ pages/api/sendmail.js
@@ -0,0 +1,38 @@
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_SERVER_HOST,
+    port: process.env.EMAIL_SERVER_PORT,
+    auth: {
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD
+    },
+    secure: true,
+})
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  const { name, email, message } = req.body;
+
+  const mailMsg = `A message was sent to you from ${name} via your portfolio website from this mail address ${email}
+  The content is as below:
+  ${message}
+  `
+  const mailOptions = {
+    from: email,
+    to: "adegbiteismail27@gmail.com",
+    subject: `New message from ${name} ${email}`,
+    text: mailMsg,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        res.status(422).json({message: 'unable to deliver mail'})
+    }
+    else {
+        res.status(200).json({message: 'email delivered successfully'})
+    }
+  })
+}
