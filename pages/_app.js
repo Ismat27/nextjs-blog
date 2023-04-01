b8b5fd0038d4b9b528eef0cc0f1c0d7a2509c3da
ismat27 <adegbiteismail27@gmail.com>
2023-04-01 22:14:07 +0100

    wrapped app with toastihy container for displaying notifications

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
+import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/global.css';
 import '../styles/prism-style.css';
 import { SSRProvider } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
 
 export default function App({ Component, pageProps }) {
     return (
         <SSRProvider>
             <Component {...pageProps} />
+            <ToastContainer />
         </SSRProvider>
     );
 }
