cc9e24d1de78f3015702c0c7130e0ee73647aec5
ismat27 <adegbiteismail27@gmail.com>
2023-04-04 10:28:29 +0100

    added project links

--- projects_data.js
+++ projects_data.js
@@ -4,35 +4,40 @@ const projects_data = [
         image: '/images/smart_farm_tool.png',
         title: 'Smart Farm Tools',
         intro: "A Platform for plants' diseases diagnosis and to connect farmers with agricultural experts for seemless consultancy services",
-        tools: ['React', 'Styled-Compnennts']
+        tools: ['React', 'Styled-Compnennts'],
+        link: 'https://smart-farm-tools.netlify.app/',
     },
     {
         id: '2',
         image: '/images/smart_mart.png',
         title: 'Smart Mart',
         intro: 'Modern day ecommerce web application for shopping different kinds of goods and commodities',
-        tools: ['React','Styled-Components']
+        tools: ['React','Styled-Components', 'Node', 'Express', 'Mongoose'],
+        link: 'https://smart-mart.netlify.app/',
     },
     {
         id: '3',
         image: '/images/where_in_the_world.png',
         title: 'Where in the world',
         intro: 'This is an app for obtaining information on  countries demograhic data',
-        tools: ['NextJs']
+        tools: ['NextJs'],
+        link: 'https://countries-info-psi.vercel.app/',
     },
     {
         id: '4',
         image: '/images/quizzical_home.png',
         title: 'Quzzical',
         intro: 'A quiz app, intended to serve user as a means of passing time while also catching fun.',
-        tools: ['React']
+        tools: ['React'],
+        link: 'https://smart-quizzical.netlify.app/',
     },
     {
         id: '5',
         image: '/images/to_do_app.png',
         title: 'TODO',
         intro: 'A minimal TODO app for listing and updating tasks to be completed',
-        tools: ['React']
+        tools: ['React'],
+        link: 'https://smarttodo.netlify.app/',
     },
 ]
 
